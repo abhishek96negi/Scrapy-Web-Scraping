93252a8deeafaf378f0d47cc0f429705bf539a69
Abhishek Negi <abhishek96negi@Abhisheks-MacBook-Air.local>
2022-02-02 21:47:58 +0530

    Whisky Shop

---
+++ whiskyshop/whiskyshop/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class WhiskyshopItem(scrapy.Item):
+    # define the fields for your item here like:
+    # whisky_name = scrapy.Field()
+    # whisky_image = scrapy.Field()
+    # whisky_price = scrapy.Field()
+    pass
